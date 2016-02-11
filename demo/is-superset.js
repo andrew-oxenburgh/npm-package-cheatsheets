041acc03253428644824b7903c9f824dceca4f6e
andrew-oxenburgh <andrew.oxenburgh@gmail.com>
2016-02-12 09:30:20 +1300

    imp:added is super object

---
+++ demo/is-superset.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var should = require('should');
+
+var isSuperset = function (subset, superset) {
+    var isNonNullObj = function (sset) {
+        return sset === null || typeof sset !== 'object';
+    };
+
+    if(isNonNullObj(superset) || isNonNullObj(subset)){
+        return false;
+    }
+
+    for(var name in subset) {
+        if (subset.hasOwnProperty(name)) {
+            if (superset[name] !== subset[name]) {
+                return false;
+            }
+        }
+    }
+    return true;
+};
+
+var tst = isSuperset;
+
+
+/*
+
+An object may be returned with superfluous properties. I want to make sure all of the properties in a creator object exist in the created object
+
+ */
+
+describe('i want a method to check one Object has all the properties of another one.', function () {
+    it('sets are equal', function () {
+        tst({}, {}).should.be.True();
+        tst({w:1}, {w:1}).should.be.True();
+    });
+
+    it('extra properties are ignored', function () {
+        tst({}, {w:2}).should.be.True();
+        tst({w:1}, {w:1, x:7}).should.be.True();
+    });
+
+    it('errors', function () {
+        tst({w:1}, {w:'1'}).should.be.False('different types an error');
+        tst({w:1}, {w:2}).should.be.False('different values should be an error');
+        tst({w:1}, {x:7}).should.be.False('same number of args, same value, different key');
+    });
+
+    it('edge cases', function () {
+        tst(null, {}).should.be.False();
+        tst({}, null).should.be.False();
+        tst(null, null).should.be.False();
+        tst('s', 's').should.be.False();
+        tst(1, 1).should.be.False();
+    });
+});
